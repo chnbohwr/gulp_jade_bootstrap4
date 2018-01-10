c0d70cf5efcb7a7105bc88eb91c3eac1c198463f
chnbohwr <chnbohwr@gmail.com>
2018-01-10 15:10:04 +0800

    add jade template

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
-# Bootstrap 4 Starter Pack (ALPHA 4.0.0-6)
-
-Includes complete Bootstrap 4.0.0-alpha6 dev environment with gulp and sass
-
-### Version
-
-1.0.0
+# Bootstrap 4 Starter Pack
 
 ## Install Dependencies
 
 ```bash
-npm install 
+npm install && npm install -g gulp
 ```
 
 ## Compile Sass & Run Dev Server
 
 ```bash
-npm start
+gulp server
 ```
 
-Files are compiled into /src
+## build dist file
+
+```bash
+gulp
+```
+
+Files are compiled into /pages
--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,57 @@
-const gulp        = require('gulp');
+const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-const sass        = require('gulp-sass');
+const sass = require('gulp-sass');
+const jade = require('gulp-jade');
+const distFolder = 'pages';
 
 // Compile Sass & Inject Into Browser
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
         .pipe(sass())
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest(`${distFolder}/css`))
         .pipe(browserSync.stream());
 });
 
 // Move JS Files to src/js
-gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
-        .pipe(gulp.dest("src/js"))
+gulp.task('js', function () {
+    return gulp.src([
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/tether/dist/js/tether.min.js',
+        'node_modules/axios/dist/axios.min.js',
+        'src/js/*.js'
+    ])
+        .pipe(gulp.dest(`${distFolder}/js`))
         .pipe(browserSync.stream());
 });
 
 // Watch Sass & Serve
-gulp.task('serve', ['sass'], function() {
-
+gulp.task('server', ['default'], function () {
     browserSync.init({
-        server: "./src"
+        server: `./${distFolder}`
     });
 
     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("src/**/*.jade", ['template']);
 });
 
 // Move Fonts to src/fonts
-gulp.task('fonts', function() {
-  return gulp.src('node_modules/font-awesome/fonts/*')
-    .pipe(gulp.dest('src/fonts'))
+gulp.task('fonts', function () {
+    return gulp.src('node_modules/font-awesome/fonts/*')
+        .pipe(gulp.dest(`${distFolder}/fonts`))
 })
 
 // Move Font Awesome CSS to src/css
-gulp.task('fa', function() {
-  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-    .pipe(gulp.dest('src/css'))
-})
+gulp.task('fa', () => gulp
+    .src('node_modules/font-awesome/css/font-awesome.min.css')
+    .pipe(gulp.dest(`${distFolder}/css`))
+)
 
-gulp.task('default', ['js','serve', 'fa', 'fonts']);
+gulp.task('template', () => gulp
+    .src('src/**/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest(`${distFolder}`))
+    .pipe(browserSync.stream())
+)
+
+gulp.task('default', ['sass', 'js', 'fa', 'fonts', 'template']);
